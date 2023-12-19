305b91b8bbd9a589e087fb45f85269dcabf353a7
Marck <mercercountynj@gmail.com>
2023-12-18 19:28:39 -0500

    securestore

--- src/services/user.js
+++ src/services/user.js
@@ -1,14 +1,25 @@
 import { useToast } from "native-base";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import { axiosInstance } from '../utils/axiosInstance';
+import * as SecureStore from 'expo-secure-store';
+
+export const FETCH_USER_QUERY_KEY = "fetch_user";
 
 export function useLogin() {
   const toast = useToast();
+  const queryClient = useQueryClient();
+
   return useMutation(
     ({ email, password }) =>
       axiosInstance.post("/users/sign_in.json", { user: { email, password }}),
       {
-        onSuccess: (data) => {
+        onSuccess: async (data) => {
+          const token = data.headers.authorization;
+          await SecureStore.setItemAsync(TOKEN_QUERY_KEY, token);
+
+          queryClient.invalidateQueries([TOKEN_QUERY_KEY]);
+          queryClient.invalidateQueries([FETCH_USER_QUERY_KEY]);
+
           toast.show({ title: JSON.stringify(data?.data)});
         },
         onError: (e) => {
@@ -20,12 +31,20 @@ export function useLogin() {
 
 export function useRegister() {
   const toast = useToast();
+  const queryClient = useQueryClient();
+
   return useMutation(
     ({ email, password }) =>
       axiosInstance.post("/users.json", { user: { email, password }}),
       {
-        onSuccess: (data) => {
-          toast.show({ title: JSON.stringify(data?.data)});
+        onSuccess: async (data) => {
+          const token = data.headers.authorization;
+          await SecureStore.setItemAsync(TOKEN_QUERY_KEY, token);
+
+          queryClient.invalidateQueries([TOKEN_QUERY_KEY]);
+          queryClient.invalidateQueries([FETCH_USER_QUERY_KEY]);
+
+          toast.show({ title: "Registered" });
         },
         onError: (e) => {
           toast.show({ title: e.message });
